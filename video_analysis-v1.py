66a01be8d02049a45235ddc368f3d3937ca3744f
Amir Meimand <amirmeimand@gmail.com>
2025-01-06 11:20:30 -0800

    Add files via upload

---
+++ video_analysis-v1.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import numpy as np
+import time
+from datetime import datetime
+import os
+
+import json
+import classification as cl
+
+def create_prompt(instruction,taxonomy):
+    # 1. Read the contents of each file
+    with open(instruction, "r") as f:
+        instruction_text = f.read()
+
+    with open(taxonomy, "r") as f:
+        taxonomy_text = f.read()
+
+    # 2. Combine the contents
+    combined_text = instruction_text + "\n\n" + taxonomy_text
+    
+    return combined_text
+
+def main(flag,rows=10):
+    taxonomy = f'./main_input/iab_definitions.txt' 
+    instruction = f'./main_input/Instruction.txt'    
+    source_file = f'./main_input/data_test.csv'
+    
+    
+    output_folder = f'./outputs_{datetime.now().strftime("%Y%m%d_%H%M%S")}'
+    n_rows = rows
+   
+    
+    url_column = 'gcs_fn'
+    Id_column = 'Id'   
+
+    os.makedirs(output_folder, exist_ok=True)  # Create the directory if it doesn't exist
+    
+    prompt = create_prompt(instruction,taxonomy)    
+    
+    
+    data = pd.read_csv(source_file)
+    data = data.head(n_rows)
+          
+    
+    print('start classification....')
+    result = cl.classification(prompt, url_column,Id_column, data)    
+    print('Classification finished!')
+    print(f'analysis {data.shape[0]} videos and generated {result.shape[0]} classified vides')
+    print('------------')
+     
+    merged_df = result.merge(data, on=Id_column, how='left')
+    
+    result.to_csv(f'{output_folder}/result.csv')
+    merged_df.to_csv(f'{output_folder}/merged_result.csv')
+
+    
+
+if __name__ == "__main__":    
+    main(flag)
+
+    
