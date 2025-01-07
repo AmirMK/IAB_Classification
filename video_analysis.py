c62e5776b7e2f0ffded33eb811f90cd89512faba
Amir Meimand <amirmeimand@gmail.com>
2025-01-06 17:42:41 -0800

    Update video_analysis.py

--- video_analysis.py
+++ video_analysis.py
@@ -3,9 +3,32 @@ import numpy as np
 import time
 from datetime import datetime
 import os
+import yaml
 
 import json
 import classification as cl
+
+CONFIG = None
+
+
+def load_config(file_path="config.yaml"):
+    """
+    Load configuration from a YAML file into a global variable.
+    """
+    global CONFIG
+    if CONFIG is None:  # Load only once
+        with open(file_path, "r") as file:
+            CONFIG = yaml.safe_load(file)
+
+def get_config():
+    """
+    Access the loaded configuration.
+    """
+    global CONFIG
+    if CONFIG is None:
+        raise RuntimeError("Configuration has not been loaded. Call `load_config()` first.")
+    return CONFIG
+
 
 def create_prompt(instruction,taxonomy):
     # 1. Read the contents of each file
@@ -21,27 +44,29 @@ def create_prompt(instruction,taxonomy):
     return combined_text
 
 def main():
+    load_config()  # Load the configuration file
+    config = get_config() 
+
+    
     taxonomy = f'./main_input/iab_definitions.txt' 
     instruction = f'./main_input/Instruction.txt'    
     source_file = f'./main_input/data.csv' #input file
     
-    
     output_folder = f'./outputs_{datetime.now().strftime("%Y%m%d_%H%M%S")}'    
        
-    url_column = 'gcs_fn' #gcs url to video ads
-    Id_column = 'Id'   #Identical value per row
+    url_column = config['url_column'] #gcs url to video ads
+    Id_column = config['Id_column']   #Identical value per row
 
     os.makedirs(output_folder, exist_ok=True)  # Create the directory if it doesn't exist
     
     prompt = create_prompt(instruction,taxonomy)    
-    
     
     data = pd.read_csv(source_file)
     n_rows = rows = data.shape[0]
           
     
     print('start classification....')
-    result = cl.classification(prompt, url_column,Id_column, data)    
+    result = cl.classification(config, prompt, url_column,Id_column, data)    
     print('Classification finished!')
     print(f'analysis {data.shape[0]} videos and generated {result.shape[0]} classified vides')
     print('------------')
@@ -51,7 +76,9 @@ def main():
 
     
 
-if __name__ == "__main__":    
+if __name__ == "__main__":   
+    load_config()  # Load the configuration file
+    config = get_config() 
     main()
 
     
